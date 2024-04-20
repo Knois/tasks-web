f5a6d20a32db1166d0944dc68a2f55cf4ad2f1d6
Knois <knoisknois@gmail.com>
2024-04-21 00:09:34 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["import", "react-hooks"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external", "internal"],
+          ["parent", "sibling", "index"],
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: ["builtin"],
+      },
+    ],
+  },
+  globals: {
+    setTimeout: true,
+    clearTimeout: true,
+  },
+};
