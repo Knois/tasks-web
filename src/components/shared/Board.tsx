32a9bc601c143cebd120fbb1889b789e9741195d
Knois <knoisknois@gmail.com>
2024-05-21 21:42:45 +0300

    fix: board

--- src/components/shared/Board.tsx
+++ src/components/shared/Board.tsx
@@ -1,4 +1,5 @@
 import API from "api/api";
+import Loading from "components/shared/Loading";
 import TasksList from "components/shared/TasksList";
 import { memo, useLayoutEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
@@ -9,22 +10,41 @@ const Board = () => {
 
   const [tasks, setTasks] = useState<ITask[]>([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useLayoutEffect(() => {
     const getTasks = async () => {
       if (!groupId) {
+        setTasks([]);
         return;
       }
+
+      setIsLoading(true);
 
       try {
         const { data } = await API.getTasksByGroupId(groupId);
         setTasks(data);
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     getTasks();
   }, [groupId]);
+
+  if (!spaceId || !groupId) {
+    return <div className="board" />;
+  }
+
+  if (isLoading) {
+    return (
+      <div className="board screenbox screenbox-headed">
+        <Loading />
+      </div>
+    );
+  }
 
   return (
     <div className="board">
